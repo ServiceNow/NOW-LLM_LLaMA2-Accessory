58124c2f449d448cdedde86b54a907f33d7639eb
kelechi.ogueji <kelechi.ogueji@servicenow.com>
2024-03-19 22:03:57 -0400

    added dataloader for dpo

--- accessory/util/misc.py
+++ accessory/util/misc.py
@@ -670,7 +670,7 @@ def cached_file_from_hf(hf_path: str) -> str:
 
 def collate_fn(batch):
     out = {}
-    for k in batch.keys():
+    for k in batch[0].keys():
         out[k] = torch.stack([sample[k] for sample in batch])
     return out
         
